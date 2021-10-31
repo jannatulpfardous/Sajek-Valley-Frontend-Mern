8eb8dfed26672ec348a9326459822127c6d9bbea
jannatulpfardous <an.jannatulfardous@gmail.com>
2021-10-31 23:42:43 +0600

    sajek valley

---
+++ README.md
@@ -0,0 +1,28 @@
+live link: https://loving-hypatia-8c7625.netlify.app
+
+heroku link: https://gory-village-18910.herokuapp.com/
+
+
+
+
+### Why is sajek famous?
+Sajek valley is known for its natural environment and is surrounded by mountains, dense forest, and grassland hill tracks. Many small rivers flow through the mountains among which the Kachalong and the Machalong are notable. ... The road to Sajek has high peaks and falls.
+
+
+### Which is the best time to visit sajek ?
+
+But in the Rainy, Autumn, and Late autumn, you will feel the white clouds very closely. So, it is considered as the best time to visit Sajek Valley. There is no problem if you go another time to travel Sajek.
+
+
+### What is sajek valley known as ?
+Sajek Valley is one of the popular tourist spots in Bangladesh situated among the hills of the Kasalong range of mountains in Sajek union, Baghaichhari Upazila in Rangamati District. Sajek valley is known as the Queen of Hills & Roof of Rangamati.
+
+### What Is Tour Guiding Service ?
+Guides Are Tourism Professionals That Lead Their Guests Through The Most Interesting Parts Of Their Region. It Is Their Task To Entertain Visitors To Their Region And To Help Them To Interpret The Sights That They Are Visiting.
+
+### How do you get to sajek Valley?
+You can go to Khagrachari or Dighinala for Sajek from Chittagong. Daily 4 BTRC AC Bus left Kadamtoli, Chittagong for Khargrachari. Besides, from the Oxyzen Mor of Chittagong, the buses of Shanti Paribahan goes for Chittagong after every 1 hour. It takes 4 to 5 hours to go from Chittagong to Khagrachari
+
+
+
+
---
+++ src/carousel/Carousel.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import "./Carousel.css"
+
+const Carousel = () => {
+    return (
+        <div>
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators col-lg-6 col-md-12 col-md-12">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active ">
+                        <img className="img-car"
+
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Sajek_Valley_Rangamati_3.jpg/1280px-Sajek_Valley_Rangamati_3.jpg" class="d-block w-100" alt="..." />
+
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 style={{ color: "white", fontFamily: "Besley" }}>"Sajek Valley Resort:</h1>
+                            <p style={{ color: "white", fontFamily: "Besley" }}> "A Unique Tourist Spot Our Motherland"</p>
+                            
+                            
+                            
+                        </div>
+
+                    </div>
+                    <div class="carousel-item">
+                        <img className="img-car"
+                            src="https://i.pinimg.com/originals/ba/c2/e8/bac2e850e80ae97e7f3cba63a730626c.jpg" class="d-block w-100" alt="..." />
+                        
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 style={{ color: "white", fontFamily: "Besley" }}>“Sajek Valley Resort:</h1>
+                            <p style={{ color: "white", fontFamily: "Besley" }}> "Journey Through Sajek Valley of Bangladesh Our Motherland"</p>
+                           
+                            
+                        </div>
+
+                    </div>
+                    <div class="carousel-item">
+                        <img className="img-car"
+                            src="https://www.bdbooking.com/upload/property/hotel/111220200305546335fad090287150.jpg" class="d-block w-100" alt="..." />
+                        
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 style={{ color: "white", fontFamily: "Besley" }}>"Sajek Valley Resort:</h1>
+                            <p style={{ color: "white", fontFamily: "Besley" }}> Discovering The Land Of Hills And Clouds"</p>
+                           
+                            
+                        </div>
+
+                    </div>
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Carousel;
